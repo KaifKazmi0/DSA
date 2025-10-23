f25bee66238951898e6ab024d35d3f22b19ef733
KaifKazmi0 <sayedkaif784@gmail.com>
2025-10-23 14:58:09 +0530

    Update PrefixSums.java

--- Arrays/PrefixSums.java
+++ Arrays/PrefixSums.java
@@ -1,1 +1,12 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class PrefixSums {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int arr[] = {1,5,7,8,4};
+      int pref[] = new int[arr.length];        
+    }
+}
 
