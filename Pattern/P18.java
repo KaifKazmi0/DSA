fb30ccdfd94e1c30447755035425ec9b2ebc1981
Kaif Kazmi <sayedkaif784@gmail.com>
2025-11-25 13:00:17 +0530

    P10 to P18 adding except P17

---
+++ Pattern/P18.java
@@ -0,0 +1,14 @@
+package Pattern;
+
+public class P18 {
+    public static void main(String[] args) {
+        int n =5 , a = 69;
+        for (int i=1;i<=n;i++){
+            for (int j=1;j<=i;j++){
+                System.out.print(((char)a++)+" ");
+            }
+            a = 69 -i;
+            System.out.println();
+        }
+    }
+}
--- Pattern/P9.java
+++ Pattern/P9.java
@@ -3,29 +3,17 @@ package Pattern;
 public class P9 {
     public static void main(String[] args) {
         int n = 5;
+        for (int i=1;i<=2*n;i++){
+            int row = (i<=n )?i: 2*n-i+1;
 
-        for (int i = 1; i <= 2*n; i++) {
-
-            if(i<=n){
-                for (int j = n-i+1;j>=1;j--){
-                    System.out.print(" ");
-                }
-
-                for(int k = 1;k<=2*i-1;k++){
-                    System.out.print("*");
-                }
-                System.out.println();
+            for (int j=1;j<=n-row;j++){
+                System.out.print(" ");
             }
-            else {
-                for (int j=1;j<=i-n;j++){
-                    System.out.print(" ");
-                }
 
-                for(int k=1;k<=2*n-(i%n);k++){
-                    System.out.print("*");
-                }
-                System.out.println();
+            for (int k=1;k<=2*row-1;k++){
+                System.out.print("*");
             }
+            System.out.println();
         }
     }
 }
