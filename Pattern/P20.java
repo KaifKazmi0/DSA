74e278a13b8146f1f6da166a1e2671ec5a279b0b
Kaif Kazmi <sayedkaif784@gmail.com>
2025-11-26 13:50:28 +0530

    New Pattern problem adding

---
+++ Pattern/P20.java
@@ -0,0 +1,21 @@
+package Pattern;
+
+public class P20 {
+    public static void main(String[] args) {
+        int n = 5;
+        int row;
+        for (int i=1;i<=2*n-1;i++){
+            row = (i<n)?i:2*n-i;
+            for (int j=1;j<=row;j++){
+                System.out.print("*");
+            }
+            for (int k=(2*n)-(2*row-1);k>1;k--){
+                System.out.print(" ");
+            }
+            for (int j=1;j<=row;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
